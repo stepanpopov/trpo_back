ce7df3aa277070e5d527e15ac89da947d4808ecf
Stepan Popov <jomolun@yandex.com>
2025-10-21 03:24:45 +0300

    F1-5: Tables interface changed to concrete instance

--- internal/pkg/track/track.go
+++ internal/pkg/track/track.go
@@ -2,19 +2,18 @@ package track
 
 import "github.com/go-park-mail-ru/2023_1_Technokaif/internal/models"
 
-//go:generate mockgen -source=track.go -destination=mocks/mock.go
-
 // Usecase includes bussiness logics methods to work with tracks
 type Usecase interface {
 	Create(track models.Track, artistsID []uint32, userID uint32) (uint32, error)
 	GetByID(trackID uint32) (*models.Track, error)
-	// Change(track models.Track) error
+	Change(track models.Track) error
 	Delete(trackID uint32, userID uint32) error
 	GetFeed() ([]models.Track, error)
 	GetByAlbum(albumID uint32) ([]models.Track, error)
 	GetByArtist(artistID uint32) ([]models.Track, error)
 	GetLikedByUser(userID uint32) ([]models.Track, error)
 	SetLike(trackID, userID uint32) (bool, error)
+	UnLike(trackID, userID uint32) (bool, error)
 	// GetListens(trackID uint32) (uint64, error)
 	// IncrementListens(trackID uint32) error
 }
@@ -30,6 +29,7 @@ type Repository interface {
 	GetByArtist(artistID uint32) ([]models.Track, error)
 	GetLikedByUser(userID uint32) ([]models.Track, error)
 	InsertLike(trackID, userID uint32) (bool, error)
+	DeleteLike(trackID, userID uint32) (bool, error)
 	// GetListens(trackID uint32) (uint64, error)
 	// IncrementListens(trackID uint32) error
 }
