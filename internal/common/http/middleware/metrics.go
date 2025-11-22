66ad6d2a29984568a8998e8a05951f3e4a1b7ad9
Stepan Popov <jomolun@yandex.com>
2025-11-22 12:33:07 +0300

    Merge branch 'F2-2_Microservices' into F2-4_Metrics

---
+++ internal/common/http/middleware/metrics.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	commonHTTP "github.com/go-park-mail-ru/2023_1_Technokaif/internal/common/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var responseTimeMetrics = promauto.NewSummaryVec(
+	prometheus.SummaryOpts{
+		Namespace:  "fluire",
+		Subsystem:  "api",
+		Name:       "response_time",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01},
+	},
+	[]string{"method", "route", "code"},
+)
+
+func observeResponseTime(duration time.Duration, method, route, code string) {
+	responseTimeMetrics.WithLabelValues(method, route, code).
+		Observe(float64(duration.Microseconds()))
+}
+
+func Metrics() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() {
+				// API route (URL Pattern)
+				routePattern := chi.RouteContext(r.Context()).RoutePattern()
+
+				// Status code
+				code := commonHTTP.GetResponseCodeFromRequest(r)
+				codeStr := ""
+				if code != 0 {
+					codeStr = strconv.Itoa(code)
+				}
+
+				observeResponseTime(time.Since(start), r.Method, routePattern, codeStr)
+			}()
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
